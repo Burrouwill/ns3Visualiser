80bcac8f9832e522129300c84f1782609c412f24
Burrouwill <wgr.burroughs@gmail.com>
2023-10-30 12:24:11 +1300

    Started using MUI for display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Paper } from '@mui/material';
 import XmlUpload from './components/xmlUpload';
-import Visualisation from './components/visualiser'; 
+import Visualisation from './components/visualiser';
 
 function App() {
+  const theme = createTheme();
+
   const [parsedData, setParsedData] = useState(null);
 
   const handleParsedData = (nodesData, simulationData) => {
@@ -12,14 +16,25 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Network Simulation</h1>
-      <XmlUpload onDataParsed={handleParsedData} />
-      <Visualisation data={parsedData} /> {/* Pass the parsed data to Visualization */}
-      <h2>NotAHEader</h2>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Network Simulation</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <XmlUpload onDataParsed={handleParsedData} />
+        </Paper>
+        <div className="visualizer-container" style={{ marginTop: '20px' }}>
+          <Visualisation data={parsedData} /> 
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
 
--- src/components/visualiser.js
+++ src/components/visualiser.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 
 function Visualization({ data }) {
-  if (!data) {
-    return <div>Select an XML file to parse and visualize.</div>;
-  }
+  const svgRef = useRef();
 
-  // Render your visualization based on the parsed data
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    if (!data) {
+      svg.selectAll('*').remove();
+      return;
+    }
+
+    // Your D3.js code to render nodes based on the data
+    data.nodesData.node.forEach((node) => {
+      const cx = parseFloat(node.$.locX);
+      const cy = parseFloat(node.$.locY);
+
+      svg.append('circle')
+        .attr('cx', cx)
+        .attr('cy', cy)
+        .attr('r', 10)
+        .attr('fill', 'green');
+    });
+
+
+    // You can add more visualization logic as needed
+  }, [data]);
+
   return (
     <div>
       <h2>Visualization</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
 
 export default Visualization;
+
